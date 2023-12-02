ae89b6b9d41026546a497d187d419d13fe9ba42f
itchytummy <broanmol17@gmail.com>
2023-12-02 16:31:00 +0530

    exam prep v1

--- sem3/Assignment2_15_08_23/cgi/3_orderedlist.py
+++ sem3/Assignment2_15_08_23/cgi/3_orderedlist.py
@@ -4,17 +4,25 @@ import cgi
 
 form = cgi.FieldStorage()
 
-name1=form.getvalue("name1")
-name2=form.getvalue("name2")
-name3=form.getvalue("name3")
+#name1=form.getvalue("name1")
+#name2=form.getvalue("name2")
+#name3=form.getvalue("name3")
+names=list()
+
+for i in range(1,4):
+	para="name"+str(i)
+	names.append(form.getvalue(para))
 
 print("Content-type: text/html \n\n")
 print("<html>")
 print("<body>")
 print("<ol>")
-print("<li>",name1,"</li>")
-print("<li>",name2,"</li>")
-print("<li>",name3,"</li>")
+for name in names: 
+	print("<li>",name,"</li>")
+
+#print("<li>",name1,"</li>")
+#print("<li>",name2,"</li>")
+#print("<li>",name3,"</li>")
 print("</ol>")
 print("</body>")
 print("</html>")
